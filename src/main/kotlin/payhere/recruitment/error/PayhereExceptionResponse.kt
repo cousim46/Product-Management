7d8307fc631e966ee907fd52f3b6180e6dc80a3b
jhw1015 <jhw1015@bank-mall.co.kr>
2024-02-07 22:54:11 +0900

    feat : 커스텀 예외 Response 처리

---
+++ src/main/kotlin/payhere/recruitment/error/PayhereExceptionResponse.kt
@@ -0,0 +1,23 @@
+package payhere.recruitment.error
+
+import org.springframework.http.ResponseEntity
+import payhere.demo.app.common.CommonResponse
+import payhere.demo.app.common.Meta
+import payhere.demo.error.PayhereErrorCode
+
+class PayhereExceptionResponse {
+
+    companion object {
+        fun toResponse(errorCode: PayhereErrorCode): ResponseEntity<CommonResponse> {
+            return ResponseEntity.status(errorCode.status)
+                .body(
+                    CommonResponse.toResponse(
+                        Meta.of(
+                            code = errorCode.statusValue(),
+                            message = errorCode.message,
+                        )
+                    )
+                )
+        }
+    }
+}
