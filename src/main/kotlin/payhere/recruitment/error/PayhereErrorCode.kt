08ab8e89000a9c630fbfa9600390cefa216fe680
cousim46 <wjdghldns24@naver.com>
2024-02-07 23:21:13 +0900

    feat : 핸드폰 번호에 대한 에러코드 추가

--- src/main/kotlin/payhere/recruitment/error/PayhereErrorCode.kt
+++ src/main/kotlin/payhere/recruitment/error/PayhereErrorCode.kt
@@ -6,7 +6,7 @@ enum class PayhereErrorCode(
     val status: HttpStatus,
     val message: String,
 ) {
-   ;
+   VIOLATION_PHONE_CONSTRAINTS(HttpStatus.BAD_REQUEST,"핸드폰 번호는 11글자여야 합니다.");
     fun statusValue(): Int {
         return status.value()
     }
