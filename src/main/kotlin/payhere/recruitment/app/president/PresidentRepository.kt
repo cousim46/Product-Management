6967314a6e37e605334e93a03318dad217b60400
cousim46 <wjdghldns24@naver.com>
2024-02-07 23:49:55 +0900

    feat : President에 대한 레파지토리 생성

---
+++ src/main/kotlin/payhere/recruitment/app/president/PresidentRepository.kt
@@ -0,0 +1,7 @@
+package payhere.recruitment.app.president
+
+import org.springframework.data.jpa.repository.JpaRepository
+import payhere.recruitment.app.president.domain.President
+
+interface PresidentRepository : JpaRepository<President, Long> {
+}
