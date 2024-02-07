138c4bbe81bb961f6560edf26130fb286890f2c7
cousim46 <wjdghldns24@naver.com>
2024-02-07 23:36:16 +0900

    fix : 불필요한 import 삭제

--- src/test/kotlin/payhere/recruitment/app/president/domain/PresidentTest.kt
+++ src/test/kotlin/payhere/recruitment/app/president/domain/PresidentTest.kt
@@ -1,7 +1,6 @@
 package payhere.recruitment.app.president.domain
 
 
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
