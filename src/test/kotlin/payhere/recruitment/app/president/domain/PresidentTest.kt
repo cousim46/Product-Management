cdc96318d06d7fa5f7c4f4c0030736e6209d0945
cousim46 <wjdghldns24@naver.com>
2024-02-07 23:35:33 +0900

    test : 핸드폰 번호가 11글자 미만일 경우에 대한 테스트 코드

--- src/test/kotlin/payhere/recruitment/app/president/domain/PresidentTest.kt
+++ src/test/kotlin/payhere/recruitment/app/president/domain/PresidentTest.kt
@@ -32,4 +32,26 @@ class PresidentTest {
         assertEquals("핸드폰 번호는 11글자여야 합니다.",errorCode.message)
         assertEquals(HttpStatus.BAD_REQUEST,errorCode.status)
     }
+
+    @Test
+    @DisplayName("핸드폰 번호가 11글자 미만이면 예외가 발생한다.")
+    fun occurPhoneLengthLessThan11Exception() {
+        //given
+        val phone = "1".repeat(10)
+        val password = "password"
+        val salt = "salt"
+
+        //when
+        val errorCode = assertThrows<PayhereException> {
+            President(
+                phone = phone,
+                password = password,
+                salt = salt
+            )
+        }.errorCode
+
+        //then
+        assertEquals("핸드폰 번호는 11글자여야 합니다.",errorCode.message)
+        assertEquals(HttpStatus.BAD_REQUEST,errorCode.status)
+    }
 }
