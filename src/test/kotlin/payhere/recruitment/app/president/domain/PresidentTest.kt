9a91587bc832bf8c156e02c90ee3b2431f96e6cf
cousim46 <wjdghldns24@naver.com>
2024-02-09 02:59:05 +0900

    test : 핸드폰 번호가 011로 시작할 경우에 대한 테스트코드

--- src/test/kotlin/payhere/recruitment/app/president/domain/PresidentTest.kt
+++ src/test/kotlin/payhere/recruitment/app/president/domain/PresidentTest.kt
@@ -54,4 +54,84 @@ class PresidentTest {
         assertEquals("핸드폰 번호는 11글자여야 합니다.",errorCode.message)
         assertEquals(HttpStatus.BAD_REQUEST,errorCode.status)
     }
+
+    @Test
+    @DisplayName("핸드폰 번호 앞자리가 011이 아니고 핸드폰 번호가 11글자이면 사장님을 생성한다.")
+    fun occur010PhoneLengthEquals11Create() {
+        //given
+        val phone = "01012341234"
+        val password = "password"
+        val salt = RandomNumber.create()
+
+        //when
+        val president = President(phone = phone, password = password, salt = salt)
+
+        //then
+        assertEquals(phone, president.phone)
+        assertEquals(password, president.password)
+        assertEquals(salt, president.salt)
+    }
+
+
+    @Test
+    @DisplayName("핸드폰 번호가 011로 시작할 경우 핸드폰 번호가 10글자를 초과하면 예외가 발생한다.")
+    fun occur011PhoneLengthExceed11Exception() {
+        //given
+        val phone = "01112341234"
+        val password = "password"
+        val salt = RandomNumber.create()
+
+        //when
+        val errorCode = assertThrows<PayhereException> {
+            President(
+                phone = phone,
+                password = password,
+                salt = salt
+            )
+        }.errorCode
+
+        //then
+        assertEquals("핸드폰 번호는 10글자여야 합니다.",errorCode.message)
+        assertEquals(HttpStatus.BAD_REQUEST,errorCode.status)
+    }
+
+    @Test
+    @DisplayName("핸드폰 번호가 011로 시작할 경우 핸드폰 번호가 10글자 미만이면 예외가 발생한다.")
+    fun occur011PhoneLengthLessThan11Exception() {
+        //given
+        val phone = "011123412"
+        val password = "password"
+        val salt = RandomNumber.create()
+
+        //when
+        val errorCode = assertThrows<PayhereException> {
+            President(
+                phone = phone,
+                password = password,
+                salt = salt
+            )
+        }.errorCode
+
+        //then
+        assertEquals("핸드폰 번호는 10글자여야 합니다.",errorCode.message)
+        assertEquals(HttpStatus.BAD_REQUEST,errorCode.status)
+    }
+
+
+    @Test
+    @DisplayName("핸드폰 번호가 011로 시작할 경우 핸드폰 번호가 10글자이면 사장님을 생성한다.")
+    fun occur011PhoneLengthEquals11Create() {
+        //given
+        val phone = "0111234123"
+        val password = "password"
+        val salt = RandomNumber.create()
+
+        //when
+        val president = President(phone = phone, password = password, salt = salt)
+
+        //then
+        assertEquals(phone, president.phone)
+        assertEquals(password, president.password)
+        assertEquals(salt, president.salt)
+    }
 }
