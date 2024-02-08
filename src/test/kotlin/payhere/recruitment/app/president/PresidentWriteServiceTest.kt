ddf75d51c185132fe83498cdd7a3670309b3dd14
cousim46 <wjdghldns24@naver.com>
2024-02-09 03:10:16 +0900

    test : 불필요한 import문 삭제

--- src/test/kotlin/payhere/recruitment/app/president/PresidentWriteServiceTest.kt
+++ src/test/kotlin/payhere/recruitment/app/president/PresidentWriteServiceTest.kt
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import payhere.recruitment.app.president.domain.President
 import payhere.recruitment.error.PayhereException
 import payhere.recruitment.app.president.dto.request.RandomNumber
 
