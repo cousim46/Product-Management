f737aaf833557e7e9ba274ca7121a78daa7ebb6b
cousim46 <wjdghldns24@naver.com>
2024-02-09 04:15:37 +0900

    feat : 핸드폰 번호로 사장님 찾아오는 쿼리메서드 추가

--- src/main/kotlin/payhere/recruitment/app/president/PresidentRepository.kt
+++ src/main/kotlin/payhere/recruitment/app/president/PresidentRepository.kt
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import payhere.recruitment.app.president.domain.President
 
 interface PresidentRepository : JpaRepository<President, Long> {
+
+    fun findByPhone(phone: String): President?
 }
