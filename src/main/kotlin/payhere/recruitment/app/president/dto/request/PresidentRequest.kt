f831f6658a1bcce80b5b09aab24e433c110bfbd6
cousim46 <wjdghldns24@naver.com>
2024-02-09 00:01:20 +0900

    feat : 사장님 회원가입에 대한 Request DTO

--- src/main/kotlin/payhere/recruitment/app/president/dto/request/PresidentRequest.kt
+++ src/main/kotlin/payhere/recruitment/app/president/dto/request/PresidentRequest.kt
@@ -1,6 +1,11 @@
 package payhere.recruitment.app.president.dto.request
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import payhere.recruitment.error.PayhereErrorCode.VIOLATION_PHONE_LENGTH_CONSTRAINTS
+import payhere.recruitment.error.PayhereErrorCode.VIOLATION_PHONE_REGEX_CONSTRAINTS
+import payhere.recruitment.error.PayhereException
 import java.security.SecureRandom
+import java.util.regex.Pattern
 
 class RandomNumber {
     companion object {
@@ -8,6 +13,25 @@ class RandomNumber {
             var secureRandom = SecureRandom();
             secureRandom.setSeed(100_000)
             return secureRandom.nextInt(999_999)
+        }
+    }
+}
+
+data class PresidentCreate(
+    @JsonProperty("phone")
+    val phone: String,
+    val password: String,
+) {
+    companion object {
+        const val PHONE_CONSTRAINTS_LENGTH = 11
+        const val PHONE_REGEX = "^010[0-9]{8}\$"
+    }
+    init {
+        require(phone.length == PHONE_CONSTRAINTS_LENGTH) {
+            throw PayhereException(VIOLATION_PHONE_LENGTH_CONSTRAINTS)
+        }
+        require(Pattern.matches(PHONE_REGEX, phone)) {
+            throw PayhereException(VIOLATION_PHONE_REGEX_CONSTRAINTS)
         }
     }
 }
