b8e710ad4814d8a27d88feb22a1cd93a4269ac2d
cousim46 <wjdghldns24@naver.com>
2024-02-09 00:04:56 +0900

    test: 사장님 회원가입시 핸드폰번호 11글자일경우에 대한 테스트 코드

--- src/test/kotlin/payhere/recruitment/app/president/PresidentWriteServiceTest.kt
+++ src/test/kotlin/payhere/recruitment/app/president/PresidentWriteServiceTest.kt
@@ -38,4 +38,39 @@ class PresidentWriteServiceTest(
         Assertions.assertEquals("핸드폰 번호는 11글자여야 합니다.", errorCode.message)
         Assertions.assertEquals(HttpStatus.BAD_REQUEST, errorCode.status)
     }
+    @Test
+    @DisplayName("핸드폰 번호가 11글자 미만이면 예외가 발생한다.")
+    fun occurPhoneLengthLessThan11Exception() {
+        //given
+        val phone = "1".repeat(10)
+        val password = "payhere"
+        val salt = RandomNumber.create()
+
+        //when
+        val errorCode = assertThrows<PayhereException> {
+            presidentWriteService.create(phone = phone, password = password, salt = salt)
+        }.errorCode
+
+        //then
+        Assertions.assertEquals("핸드폰 번호는 11글자여야 합니다.", errorCode.message)
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, errorCode.status)
+    }
+
+
+    @Test
+    @DisplayName("핸드폰 번호가 11글자이면 사장님을 생성한다.")
+    fun occurPhoneLengthEquals11Create() {
+        //given
+        val phone = "1".repeat(11)
+        val password = "payhere"
+        val salt = RandomNumber.create()
+
+        //when
+        val savePresident = presidentWriteService.create(phone = phone, password = password, salt = salt)
+
+        //then
+        val findPresident = presidentRepository.findById(savePresident.id).get()
+        Assertions.assertEquals(savePresident.phone, findPresident.phone)
+        Assertions.assertEquals(savePresident.salt, findPresident.salt)
+    }
 }
