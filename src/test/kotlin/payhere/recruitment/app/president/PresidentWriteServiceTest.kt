18a67f68db93ab21f12690b165ed661afbcce8b0
cousim46 <wjdghldns24@naver.com>
2024-02-09 03:09:51 +0900

    test : 핸드폰 번호가 011로 시작할 경우에 대한 테스트코드 및 테스트 코드 메서드명 변경, 설명 변경

--- src/test/kotlin/payhere/recruitment/app/president/PresidentWriteServiceTest.kt
+++ src/test/kotlin/payhere/recruitment/app/president/PresidentWriteServiceTest.kt
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import payhere.recruitment.app.president.domain.President
 import payhere.recruitment.error.PayhereException
 import payhere.recruitment.app.president.dto.request.RandomNumber
 
@@ -24,33 +25,16 @@ class PresidentWriteServiceTest(
     }
 
     @Test
-    @DisplayName("핸드폰 번호가 11글자 이상이면 예외 발생한다.")
-    fun occurPhoneLengthExceed11Exception() {
+    @DisplayName("핸드폰 번호가 11글자를 초과하면 예외가 발생한다.")
+    fun occur010PhoneLengthExceed11Exception() {
         //given
-        val phone = "1".repeat(12)
-        val salt = RandomNumber.create()
-        val password = "payhere"
-
-        //when
-        val errorCode = assertThrows<PayhereException> {
-            presidentWriteService.create(phone = phone, password = password, salt = salt)
-        }.errorCode
-
-        //then
-        Assertions.assertEquals("핸드폰 번호는 11글자여야 합니다.", errorCode.message)
-        Assertions.assertEquals(HttpStatus.BAD_REQUEST, errorCode.status)
-    }
-    @Test
-    @DisplayName("핸드폰 번호가 11글자 미만이면 예외가 발생한다.")
-    fun occurPhoneLengthLessThan11Exception() {
-        //given
-        val phone = "1".repeat(10)
-        val password = "payhere"
+        val phone = "010123412341"
+        val password = "password"
         val salt = RandomNumber.create()
 
         //when
         val errorCode = assertThrows<PayhereException> {
-            presidentWriteService.create(phone = phone, password = password, salt = salt)
+            presidentWriteService.create(phone = phone, password = password, salt =salt)
         }.errorCode
 
         //then
@@ -58,29 +42,100 @@ class PresidentWriteServiceTest(
         Assertions.assertEquals(HttpStatus.BAD_REQUEST, errorCode.status)
     }
 
-
     @Test
-    @DisplayName("핸드폰 번호가 11글자이면 사장님을 생성한다.")
-    fun occurPhoneLengthEquals11Create() {
+    @DisplayName("핸드폰 번호 앞자리가 011이 아닐경우 핸드폰 번호가 11글자 미만이면 예외가 발생한다.")
+    fun occur010PhoneLengthLessThan11Exception() {
         //given
-        val phone = "1".repeat(11)
-        val password = "payhere"
+        val phone = "0101234123"
+        val password = "password"
         val salt = RandomNumber.create()
 
         //when
-        val savePresident = presidentWriteService.create(phone = phone, password = password, salt = salt)
+        val errorCode = assertThrows<PayhereException> {
+            presidentWriteService.create(phone = phone, password = password, salt =salt)
+        }.errorCode
 
         //then
-        val findPresident = presidentRepository.findById(savePresident.id).get()
-        Assertions.assertEquals(savePresident.phone, findPresident.phone)
-        Assertions.assertEquals(savePresident.salt, findPresident.salt)
+        Assertions.assertEquals("핸드폰 번호는 11글자여야 합니다.", errorCode.message)
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, errorCode.status)
     }
+
+    @Test
+    @DisplayName("핸드폰 번호 앞자리가 011이 아니고 핸드폰 번호가 11글자이면 사장님을 생성한다.")
+    fun occur010PhoneLengthEquals11Create() {
+        //given
+        val phone = "01012341234"
+        val password = "password"
+        val salt = RandomNumber.create()
+
+        //when
+        val president =
+            presidentWriteService.create(phone = phone, password = password, salt = salt)
+
+        //then
+        Assertions.assertEquals(phone, president.phone)
+        Assertions.assertEquals(salt, president.salt)
+    }
+
+    @Test
+    @DisplayName("핸드폰 번호가 011로 시작할 경우 핸드폰 번호가 10글자를 초과하면 예외가 발생한다.")
+    fun occur011PhoneLengthExceed11Exception() {
+        //given
+        val phone = "01112341234"
+        val password = "password"
+        val salt = RandomNumber.create()
+
+        //when
+        val errorCode = assertThrows<PayhereException> {
+            presidentWriteService.create(phone = phone, password = password, salt =salt)
+        }.errorCode
+
+        //then
+        Assertions.assertEquals("핸드폰 번호는 10글자여야 합니다.", errorCode.message)
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, errorCode.status)
+    }
+
+    @Test
+    @DisplayName("핸드폰 번호가 011로 시작할 경우 핸드폰 번호가 10글자 미만이면 예외가 발생한다.")
+    fun occur011PhoneLengthLessThan11Exception() {
+        //given
+        val phone = "011123412"
+        val password = "password"
+        val salt = RandomNumber.create()
+
+        //when
+        val errorCode = assertThrows<PayhereException> {
+            presidentWriteService.create(phone = phone, password = password, salt =salt)
+        }.errorCode
+
+        //then
+        Assertions.assertEquals("핸드폰 번호는 10글자여야 합니다.", errorCode.message)
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, errorCode.status)
+    }
+
+
+    @Test
+    @DisplayName("핸드폰 번호가 011로 시작할 경우 핸드폰 번호가 10글자이면 사장님을 생성한다.")
+    fun occur011PhoneLengthEquals11Create() {
+        //given
+        val phone = "0111234123"
+        val password = "password"
+        val salt = RandomNumber.create()
+
+        //when
+        val president = presidentWriteService.create(phone = phone, password = password, salt =salt)
+
+        //then
+        Assertions.assertEquals(phone, president.phone)
+        Assertions.assertEquals(salt, president.salt)
+    }
+
 
     @Test
     @DisplayName("솔트암호화로 사장님이 입력한 비밀번호와 회원가입된 비밀번호가 일치하지 않는다.")
-    fun encodePassword() {
+    fun encodePasswordNotEqualInputPassword() {
         //given
-        val phone = "1".repeat(11)
+        val phone = "010123412341"
         val password = "payhere"
         val salt = RandomNumber.create()
 
@@ -94,9 +149,9 @@ class PresidentWriteServiceTest(
 
     @Test
     @DisplayName("솔트암호화로 사장님이 입력한 비밀번호와 salt 합친 값과 회원가입된 비밀번호가 일치한다.")
-    fun encodePassword2() {
+    fun encodePasswordEqualInputPassword() {
         //given
-        val phone = "1".repeat(11)
+        val phone = "010123412341"
         val password = "payhere"
         val salt = RandomNumber.create()
         val saltRawPassword = password + salt
