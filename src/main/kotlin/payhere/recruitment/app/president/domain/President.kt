94049174672d9162222ed4a93ad533c52d37630b
cousim46 <wjdghldns24@naver.com>
2024-02-09 02:53:39 +0900

    feat : 011로 시작하는 핸드폰 번호에 대한 예외처리 추가

--- src/main/kotlin/payhere/recruitment/app/president/domain/President.kt
+++ src/main/kotlin/payhere/recruitment/app/president/domain/President.kt
@@ -3,6 +3,8 @@ package payhere.recruitment.app.president.domain
 import payhere.recruitment.error.PayhereErrorCode.VIOLATION_PHONE_LENGTH_CONSTRAINTS
 import payhere.recruitment.error.PayhereException
 import payhere.recruitment.app.common.domain.BaseEntity
+import payhere.recruitment.error.PayhereErrorCode
+import payhere.recruitment.error.PayhereErrorCode.VIOLATION_PHONE_START_011_LENGTH_CONSTRAINTS
 import javax.persistence.Column
 import javax.persistence.Entity
 
@@ -17,8 +19,16 @@ class President(
 ) : BaseEntity() {
     companion object {
         const val PHONE_CONSTRAINTS_LENGTH = 11
+        const val PHONE_START_011_CONSTRAINTS_LENGTH = 10
+        const val PHONE_START_011 = "011"
     }
     init {
+        val startNumber = phone.substring(0, 3)
+        if(startNumber == PHONE_START_011) {
+            require(phone.length == PHONE_START_011_CONSTRAINTS_LENGTH) {
+                throw PayhereException(VIOLATION_PHONE_START_011_LENGTH_CONSTRAINTS)
+            }
+        }
         require(phone.length == PHONE_CONSTRAINTS_LENGTH) {
             throw PayhereException(VIOLATION_PHONE_LENGTH_CONSTRAINTS)
         }
