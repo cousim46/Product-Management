42e7b8f19ebcaf30c76a4d70cd98cd63b5555f13
cousim46 <wjdghldns24@naver.com>
2024-02-09 02:54:27 +0900

    test : 011로 시작하는 핸드폰 번호에 대한 예외코드 추가

--- src/main/kotlin/payhere/recruitment/error/PayhereErrorCode.kt
+++ src/main/kotlin/payhere/recruitment/error/PayhereErrorCode.kt
@@ -7,6 +7,7 @@ enum class PayhereErrorCode(
     val message: String,
 ) {
    VIOLATION_PHONE_LENGTH_CONSTRAINTS(HttpStatus.BAD_REQUEST,"핸드폰 번호는 11글자여야 합니다."),
+   VIOLATION_PHONE_START_011_LENGTH_CONSTRAINTS(HttpStatus.BAD_REQUEST,"핸드폰 번호는 10글자여야 합니다."),
    VIOLATION_PHONE_REGEX_CONSTRAINTS(HttpStatus.BAD_REQUEST,"핸드폰 번호가 양식에 맞지않습니다.");
     fun statusValue(): Int {
         return status.value()
