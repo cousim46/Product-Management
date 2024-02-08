a0879c7b9e514412268dcfec20d6364bbde1b7d8
cousim46 <wjdghldns24@naver.com>
2024-02-09 02:55:14 +0900

    test : 테스트코드 메서드명 변경

--- src/main/kotlin/payhere/recruitment/app/president/dto/request/PresidentRequest.kt
+++ src/main/kotlin/payhere/recruitment/app/president/dto/request/PresidentRequest.kt
@@ -1,8 +1,8 @@
 package payhere.recruitment.app.president.dto.request
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import payhere.recruitment.error.PayhereErrorCode.VIOLATION_PHONE_LENGTH_CONSTRAINTS
-import payhere.recruitment.error.PayhereErrorCode.VIOLATION_PHONE_REGEX_CONSTRAINTS
+import payhere.recruitment.error.PayhereErrorCode
+import payhere.recruitment.error.PayhereErrorCode.*
 import payhere.recruitment.error.PayhereException
 import java.security.SecureRandom
 import java.util.regex.Pattern
@@ -23,14 +23,27 @@ data class PresidentCreate(
     val password: String,
 ) {
     companion object {
-        const val PHONE_CONSTRAINTS_LENGTH = 11
-        const val PHONE_REGEX = "^010[0-9]{8}\$"
+        const val PHONE_BASIC_CONSTRAINTS_LENGTH = 11
+        const val PHONE_START_011_CONSTRAINTS_LENGTH = 10
+        const val PHONE_START_011 = "011"
+
+        const val PHONE_BASIC_REGEX = "^010[0-9]{8}\$"
+        const val PHONE_START_011_REGEX = "^011\\d{7}\$"
+
     }
     init {
-        require(phone.length == PHONE_CONSTRAINTS_LENGTH) {
-            throw PayhereException(VIOLATION_PHONE_LENGTH_CONSTRAINTS)
+        val startNumber = phone.substring(0, 3)
+
+        if(startNumber == PHONE_START_011) {
+            require(phone.length == PHONE_START_011_CONSTRAINTS_LENGTH) {
+                throw PayhereException(VIOLATION_PHONE_START_011_LENGTH_CONSTRAINTS)
+            }
+        }else {
+            require(phone.length == PHONE_BASIC_CONSTRAINTS_LENGTH) {
+                throw PayhereException(VIOLATION_PHONE_LENGTH_CONSTRAINTS)
+            }
         }
-        require(Pattern.matches(PHONE_REGEX, phone)) {
+        require(Pattern.matches(PHONE_START_011_REGEX, phone) || Pattern.matches(PHONE_BASIC_REGEX, phone) ) {
             throw PayhereException(VIOLATION_PHONE_REGEX_CONSTRAINTS)
         }
     }
