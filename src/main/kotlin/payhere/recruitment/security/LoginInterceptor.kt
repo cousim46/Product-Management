0eb8fe942d7209835ccda0142155b5a3e4979f06
cousim46 <wjdghldns24@naver.com>
2024-02-09 13:58:50 +0900

    feat : LoginUser 어노테이션이 작성된 코드에서 로그인한 유저 정보를 담는 인터셉터

---
+++ src/main/kotlin/payhere/recruitment/security/LoginInterceptor.kt
@@ -0,0 +1,28 @@
+package payhere.recruitment.security
+
+import org.springframework.core.MethodParameter
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.bind.support.WebDataBinderFactory
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.method.support.ModelAndViewContainer
+import payhere.recruitment.app.president.PresidentRepository
+import payhere.recruitment.app.president.dto.request.LoginInfo
+import payhere.recruitment.error.CommonErrorCode
+import payhere.recruitment.error.CommonException
+import payhere.recruitment.security.annotation.LoginUser
+
+class LoginInterceptor(
+    private val presidentRepository: PresidentRepository
+) : HandlerMethodArgumentResolver {
+    override fun supportsParameter(parameter: MethodParameter): Boolean {
+        return parameter.hasParameterAnnotation(LoginUser::class.java)
+    }
+
+    override fun resolveArgument(parameter: MethodParameter, mavContainer: ModelAndViewContainer?, webRequest: NativeWebRequest, binderFactory: WebDataBinderFactory?): Any? {
+        val authentication = SecurityContextHolder.getContext().authentication
+        val president = presidentRepository.findByPhone(authentication.name)
+            ?: throw CommonException(CommonErrorCode.ACCESS_TOKEN_EXPIRE)
+        return LoginInfo(president.id)
+    }
+}
