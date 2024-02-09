2cb9f10a911ca1bf6504b9e5f07a386720519377
cousim46 <wjdghldns24@naver.com>
2024-02-09 14:49:44 +0900

    feat : 로그인에 대한 API

--- src/main/kotlin/payhere/recruitment/app/manager/ManagerController.kt
+++ src/main/kotlin/payhere/recruitment/app/manager/ManagerController.kt
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import payhere.demo.app.common.CommonResponse
+import payhere.recruitment.app.manager.dto.request.Longin
 import payhere.recruitment.app.manager.dto.request.PresidentCreate
 import payhere.recruitment.app.manager.dto.request.RandomNumber
+import payhere.recruitment.token.LoginToken
+import java.util.*
+import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServletResponse
 
 @RequestMapping("/api/president")
 @RestController
@@ -21,6 +26,13 @@ class ManagerController(
             password = presidentCreate.password,
             salt = RandomNumber.create()
         )
+        return CommonResponse.toResponse()
+    }
+    @PostMapping("/login")
+    fun login(@RequestBody login: Longin, httpServletResponse: HttpServletResponse) : CommonResponse {
+        val loginToken: LoginToken = presidentWriteService.login(login.phone,login.password, Date())
+        val cookie = Cookie("loginInfo",loginToken.access)
+        httpServletResponse.addCookie(cookie)
         return CommonResponse.toResponse()
     }
 }
