9e1c852ed72f5804407bbfc7f279b89b5e4de6b0
cousim46 <wjdghldns24@naver.com>
2024-02-09 13:22:18 +0900

    feat : 로그인 유저에 대한 어노테이션

---
+++ src/main/kotlin/payhere/recruitment/security/annotation/LoginUser.kt
@@ -0,0 +1,7 @@
+package payhere.recruitment.security.annotation
+
+@Target(AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+annotation class LoginUser
+
