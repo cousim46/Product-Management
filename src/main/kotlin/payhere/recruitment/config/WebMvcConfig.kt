09ae9cfa62b7b8be3364b308a4e07264be2357b2
cousim46 <wjdghldns24@naver.com>
2024-02-09 14:37:50 +0900

    fix : president -> manager 변경

--- src/main/kotlin/payhere/recruitment/config/WebMvcConfig.kt
+++ src/main/kotlin/payhere/recruitment/config/WebMvcConfig.kt
@@ -3,16 +3,16 @@ package payhere.recruitment.config
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import payhere.recruitment.app.president.PresidentRepository
+import payhere.recruitment.app.manager.ManagerRepository
 import payhere.recruitment.security.LoginInterceptor
 
 @Configuration
 class WebMvcConfig(
-    private val presidentRepository: PresidentRepository,
+    private val managerRepository: ManagerRepository,
 ): WebMvcConfigurer {
 
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
-        resolvers.add(LoginInterceptor(presidentRepository))
+        resolvers.add(LoginInterceptor(managerRepository))
     }
 
 }
