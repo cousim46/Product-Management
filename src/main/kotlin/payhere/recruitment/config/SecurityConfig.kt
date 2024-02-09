764d84933576c958849ca7fb19e28b285de99c72
cousim46 <wjdghldns24@naver.com>
2024-02-09 14:42:16 +0900

    feat : 인증 및 인가에 대한 필터, 엔트리 포인트 시큐리티에 등록

--- src/main/kotlin/payhere/recruitment/config/SecurityConfig.kt
+++ src/main/kotlin/payhere/recruitment/config/SecurityConfig.kt
@@ -7,10 +7,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import payhere.recruitment.security.LoginFilter
+import payhere.recruitment.security.TokenAccessDeniedHandler
+import payhere.recruitment.security.annotation.TokenAuthenticationEntryPoint
+import payhere.recruitment.token.TokenProvider
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig(
+    private val tokenProvider: TokenProvider,
+    private val tokenAccessDeniedHandler: TokenAccessDeniedHandler,
+    private val tokenAuthenticationEntryPoint: TokenAuthenticationEntryPoint,
 ) : WebSecurityConfigurerAdapter() {
 
     @Bean
@@ -23,6 +31,11 @@ class SecurityConfig(
             .cors().disable()
             .csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .addFilterBefore(LoginFilter(tokenProvider,tokenAuthenticationEntryPoint), UsernamePasswordAuthenticationFilter::class.java)
+            .exceptionHandling()
+            .authenticationEntryPoint(tokenAuthenticationEntryPoint)
+            .accessDeniedHandler(tokenAccessDeniedHandler)
             .and()
             .authorizeRequests()
             .antMatchers("/**")
