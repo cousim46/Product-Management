29b0b5fa0ebe66cc569522eefa652fef93610bda
cousim46 <wjdghldns24@naver.com>
2024-02-11 01:40:28 +0900

    config : 테스트를 위한 h2 데이터베이스 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
 
+    testRuntimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 }
