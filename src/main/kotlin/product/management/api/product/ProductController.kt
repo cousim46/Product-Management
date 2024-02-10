db1e853dfc003bd870a77a547c0aa80dd879c405
cousim46 <wjdghldns24@naver.com>
2024-02-11 05:01:59 +0900

    feat : 상품정보에 상세보기에 대한 API

--- src/main/kotlin/product/management/api/product/ProductController.kt
+++ src/main/kotlin/product/management/api/product/ProductController.kt
@@ -1,13 +1,20 @@
 package product.management.api.product
 
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import product.demo.app.common.CommonResponse
+import product.demo.app.common.Data
 import product.management.api.manager.dto.request.LoginInfo
 import product.management.api.product.dto.request.ProductApiCreate
+import product.management.app.product.ProductReadService
 import product.management.app.product.ProductWriteService
+import product.management.app.product.dto.ProductDetailInfo
 import product.management.app.product.dto.ProductInfo
 import product.management.security.annotation.LoginUser
 
@@ -15,11 +22,19 @@ import product.management.security.annotation.LoginUser
 @RequestMapping("/api/product")
 class ProductController(
     private val productWriteService: ProductWriteService,
+    private val productReadService: ProductReadService,
 ) {
 
     @PostMapping
     fun create(@LoginUser loginInfo: LoginInfo, @RequestBody productApiCreate: ProductApiCreate): CommonResponse {
         productWriteService.create(id = loginInfo.id, productInfo = ProductInfo.of(productApiCreate))
         return CommonResponse.toResponse()
+    }
+
+    @GetMapping("/{product-id}")
+    fun getDetail(@LoginUser loginInfo: LoginInfo, @PathVariable("product-id") productId: Long): CommonResponse {
+        val product = productReadService.getDetail(productId = productId, managerId = loginInfo.id)
+        val data = Data.of(ProductDetailInfo.toResponse(product))
+        return CommonResponse.toResponse(data = data)
     }
 }
