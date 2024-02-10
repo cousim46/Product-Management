1174369403f79882bdd54bccc4971cdf447a715f
cousim46 <wjdghldns24@naver.com>
2024-02-11 04:40:11 +0900

    feat : 로그아웃 로직에 대한 예외처리

--- src/main/kotlin/product/management/security/LoginFilter.kt
+++ src/main/kotlin/product/management/security/LoginFilter.kt
@@ -2,6 +2,7 @@ package product.management.security
 
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
+import product.management.app.token.RefreshTokenRepository
 import product.management.security.annotation.TokenAuthenticationEntryPoint
 import product.management.token.TokenProvider
 import javax.servlet.FilterChain
@@ -10,13 +11,21 @@ import javax.servlet.http.HttpServletResponse
 
 class LoginFilter(
     private val tokenProvider: TokenProvider,
-    private val tokenAuthenticationEntryPoint: TokenAuthenticationEntryPoint
+    private val tokenAuthenticationEntryPoint: TokenAuthenticationEntryPoint,
+    private val refreshTokenRepository: RefreshTokenRepository,
 ) : OncePerRequestFilter() {
-    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
         tokenProvider.resolveToken(request = request as? HttpServletRequest?)?.also {
             val loginToken = it.split(" ")
-            if(loginToken.size != 2 || !it.toLowerCase().startsWith("bearer ") || !tokenProvider.validateToken(loginToken[1])) {
-                tokenAuthenticationEntryPoint.commence(request,response = response, null)
+            if (loginToken.size != 2 || !it.toLowerCase()
+                    .startsWith("bearer ") || !tokenProvider.validateToken(loginToken[1]) ||
+                !refreshTokenRepository.existsByAccessToken(accessToken = loginToken[1])
+                ) {
+                tokenAuthenticationEntryPoint.commence(request, response = response, null)
                 return
             }
             val authentication = tokenProvider.getAuthentication(loginToken[1])
