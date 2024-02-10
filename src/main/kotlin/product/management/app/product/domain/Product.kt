18b69fbe650a79da94833a576a3ea7b1a3fb6c18
cousim46 <wjdghldns24@naver.com>
2024-02-11 01:40:03 +0900

    feat : Product에 포함되어있는 컬럼 제약조건 추가 및 상품 등록한 manager id 반환 메서드 추가

--- src/main/kotlin/product/management/app/product/domain/Product.kt
+++ src/main/kotlin/product/management/app/product/domain/Product.kt
@@ -1,18 +1,33 @@
 package product.management.app.product.domain
 
 import product.management.app.common.domain.BaseEntity
+import product.management.app.manager.domain.Manager
 import product.management.app.product.enums.Size
 import java.time.LocalDateTime
-import javax.persistence.Entity
+import javax.persistence.*
 
 @Entity
 class Product(
+    @Column(nullable = false)
     val category: String,
+    @Column(nullable = false)
     val price: Int,
+    @Column(nullable = false)
     val name: String,
+    @Column(nullable = false)
     val explain: String,
+    @Column(nullable = false)
     val expirationDate: LocalDateTime,
+    @Column(nullable = false, unique = true)
     val barcode: String,
-    val size: Size
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    val size: Size,
+    @Embedded
+    val companyInfo: CompanyInfo,
+    @ManyToOne(fetch = FetchType.LAZY)
+    val manager: Manager,
 ) : BaseEntity() {
+
+    fun returnManagerId() = manager.id
 }
