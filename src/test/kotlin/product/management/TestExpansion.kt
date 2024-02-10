9d50f82d6337e3dc04bb068191bde38e10e8517f
cousim46 <wjdghldns24@naver.com>
2024-02-11 01:47:08 +0900

    test : 테스트에 필요한 데이터를 생성하는 확장함수 추가

---
+++ src/test/kotlin/product/management/TestExpansion.kt
@@ -0,0 +1,47 @@
+package product.management
+
+import product.management.app.manager.ManagerRepository
+import product.management.app.manager.domain.Manager
+import product.management.app.product.ProductRepository
+import product.management.app.product.domain.CompanyInfo
+import product.management.app.product.domain.Product
+import product.management.app.product.enums.Size
+import java.time.LocalDateTime
+
+
+internal fun ManagerRepository.create(
+    phone: String = "01012341234",
+    password: String = "test",
+    salt: Int = 1234,
+): Manager = this.saveAndFlush(
+    Manager(
+        phone = phone,
+        password = password,
+        salt = salt,
+    ),
+)
+
+internal fun ProductRepository.create(
+    category: String = "음료",
+    price: Int = 5000,
+    name: String = "아메리카노",
+    explain: String = "커피입니다.",
+    expirationDate: LocalDateTime = LocalDateTime.now(),
+    barcode: String,
+    size: Size = Size.SMALL,
+    companyInfo: CompanyInfo,
+    manager: Manager,
+): Product = this.saveAndFlush(
+    Product(
+        category = category,
+        price = price,
+        name = name,
+        explain = explain,
+        expirationDate = expirationDate,
+        barcode = barcode,
+        size = size,
+        companyInfo = companyInfo,
+        manager= manager,
+
+        ),
+)
