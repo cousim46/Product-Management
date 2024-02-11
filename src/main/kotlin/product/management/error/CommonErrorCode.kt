a6334c9389e18b3da5b9406ae86fc0c72bc3f4b5
cousim46 <wjdghldns24@naver.com>
2024-02-12 03:10:16 +0900

    fix : 메시지 변경 및 에러코드 추가

--- src/main/kotlin/product/management/error/CommonErrorCode.kt
+++ src/main/kotlin/product/management/error/CommonErrorCode.kt
@@ -7,8 +7,8 @@ enum class CommonErrorCode(
     val status: HttpStatus,
     val message: String,
 ) {
-   VIOLATION_PHONE_LENGTH_CONSTRAINTS(BAD_REQUEST,"핸드폰 번호는 11글자여야 합니다."),
-   VIOLATION_PHONE_START_011_LENGTH_CONSTRAINTS(BAD_REQUEST,"핸드폰 번호는 10글자여야 합니다."),
+   VIOLATION_PHONE_LENGTH_CONSTRAINTS(BAD_REQUEST,"010으로 시작하는 핸드폰 번호는 11글자여야 합니다."),
+   VIOLATION_PHONE_START_011_LENGTH_CONSTRAINTS(BAD_REQUEST,"011로 시작하는 핸드폰 번호는 10글자여야 합니다."),
    VIOLATION_PHONE_REGEX_CONSTRAINTS(BAD_REQUEST,"핸드폰 번호가 양식에 맞지않습니다."),
     NOT_MATCH_ID_OR_PASSWORD(BAD_REQUEST,"아이디 또는 비밀번호가 일치하지 않습니다."),
     BAD_REQUEST_PRODUCT_SIZE(BAD_REQUEST,"잘못된 상품 사이즈 입니다."),
@@ -22,7 +22,8 @@ enum class CommonErrorCode(
     NOT_EXSISTS_INFO(NOT_FOUND,"존재하지 않은 정보입니다."),
     NOT_EXSISTS_PRODUCT_INFO(NOT_FOUND,"존재하지 않는 상품정보입니다."),
 
-    ALREADY_EXSISTS_BARCODE(CONFLICT, "이미 존재하는 바코드입니다.");
+    ALREADY_EXSISTS_BARCODE(CONFLICT, "이미 존재하는 바코드입니다."),
+    ALREADY_EXSISTS_PHONE(CONFLICT, "이미 가입된 정보입니다.");
 
     fun statusValue(): Int {
         return status.value()
