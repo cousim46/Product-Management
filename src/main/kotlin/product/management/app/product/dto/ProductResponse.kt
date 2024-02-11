30f6c6824f5d59bb2665dd9ef5528f431a8d7b92
cousim46 <wjdghldns24@naver.com>
2024-02-11 20:07:44 +0900

    feat : 상품 목록에 대한 ResponseDTO

--- src/main/kotlin/product/management/app/product/dto/ProductResponse.kt
+++ src/main/kotlin/product/management/app/product/dto/ProductResponse.kt
@@ -1,6 +1,7 @@
 package product.management.app.product.dto
 
 import com.fasterxml.jackson.annotation.JsonFormat
+import org.springframework.data.domain.Slice
 import product.management.app.product.domain.Product
 import product.management.app.product.enums.Size
 import java.time.LocalDateTime
@@ -25,3 +26,43 @@ data class ProductDetailInfo(
         }
     }
 }
+
+data class Products(
+    val isLast: Boolean,
+    val isFirst: Boolean,
+    val limit: Int,
+    val page: Int,
+    val infos: List<ProductsInfo>
+) {
+    companion object  {
+        fun of(products: Slice<Product>) : Products {
+            return Products(
+                isLast =  products.isLast,
+                isFirst = products.isFirst,
+                page = products.number,
+                limit = products.size,
+                infos = products.content.map { ProductsInfo(
+                    productId = it.id,
+                    name = it.name,
+                    price = it.price,
+                    content = it.content,
+                    expirationDate = it.expirationDate,
+                    size = it.size,
+                    createdAt = it.createdAt
+                ) }
+            )
+        }
+    }
+}
+
+data class ProductsInfo(
+    val productId: Long,
+    val name: String,
+    val price: Int,
+    val content: String,
+    @JsonFormat(pattern = "yyyy년 MM월 dd일 HH시 mm분 ss초")
+    val expirationDate: LocalDateTime,
+    val size: Size,
+    @JsonFormat(pattern = "yyyy년 MM월 dd일 HH시 mm분 ss초")
+    val createdAt: LocalDateTime
+)
