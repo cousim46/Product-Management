a5b1c2c825819323af5531b62c173403e6d99e5b
cousim46 <wjdghldns24@naver.com>
2024-02-12 03:10:39 +0900

    fix : 핸드폰 번호 존재여부에 대한 메서드 쿼리 추가

--- src/main/kotlin/product/management/app/manager/ManagerRepository.kt
+++ src/main/kotlin/product/management/app/manager/ManagerRepository.kt
@@ -6,4 +6,5 @@ import product.management.app.manager.domain.Manager
 interface ManagerRepository : JpaRepository<Manager, Long> {
 
     fun findByPhone(phone: String): Manager?
+    fun existsByPhone(phone: String) : Boolean
 }
