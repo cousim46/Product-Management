fdffd02c9e4d289d8275682bcb40feef1977bae3
cousim46 <wjdghldns24@naver.com>
2024-02-11 20:06:48 +0900

    feat : queryDslRepository 상속

--- src/main/kotlin/product/management/app/product/ProductRepository.kt
+++ src/main/kotlin/product/management/app/product/ProductRepository.kt
@@ -3,7 +3,7 @@ package product.management.app.product
 import org.springframework.data.jpa.repository.JpaRepository
 import product.management.app.product.domain.Product
 
-interface ProductRepository : JpaRepository<Product, Long> {
+interface ProductRepository : JpaRepository<Product, Long>, ProductQueryDslRepository {
     fun existsByBarcode(barcode: String) : Boolean
 
     fun findByIdAndManagerId(productId: Long, managerId: Long): Product?
