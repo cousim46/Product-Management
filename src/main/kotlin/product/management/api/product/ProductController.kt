e3eacd9aa3b978c2a91da919490002a47e52998a
cousim46 <wjdghldns24@naver.com>
2024-02-11 20:05:17 +0900

    feat : 상품 목록 및 상품 검색 API

--- src/main/kotlin/product/management/api/product/ProductController.kt
+++ src/main/kotlin/product/management/api/product/ProductController.kt
@@ -1,10 +1,11 @@
 package product.management.api.product
 
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -12,10 +13,12 @@ import product.demo.app.common.CommonResponse
 import product.demo.app.common.Data
 import product.management.api.manager.dto.request.LoginInfo
 import product.management.api.product.dto.request.ProductApiCreate
+import product.management.api.product.dto.request.Search
 import product.management.app.product.ProductReadService
 import product.management.app.product.ProductWriteService
 import product.management.app.product.dto.ProductDetailInfo
 import product.management.app.product.dto.ProductInfo
+import product.management.app.product.dto.Products
 import product.management.security.annotation.LoginUser
 
 @RestController
@@ -26,26 +29,40 @@ class ProductController(
 ) {
 
     @PostMapping
-    fun create(@LoginUser loginInfo: LoginInfo, @RequestBody productApiCreate: ProductApiCreate): CommonResponse {
-        productWriteService.create(id = loginInfo.id, productInfo = ProductInfo.of(productApiCreate))
+    fun create(
+        @LoginUser loginInfo: LoginInfo,
+        @RequestBody productApiCreate: ProductApiCreate
+    ): CommonResponse {
+        productWriteService.create(
+            id = loginInfo.id,
+            productInfo = ProductInfo.of(productApiCreate)
+        )
         return CommonResponse.toResponse()
     }
 
     @GetMapping("/{product-id}")
-    fun getDetail(@LoginUser loginInfo: LoginInfo, @PathVariable("product-id") productId: Long): CommonResponse {
+    fun getDetail(
+        @LoginUser loginInfo: LoginInfo,
+        @PathVariable("product-id") productId: Long
+    ): CommonResponse {
         val product = productReadService.getDetail(productId = productId, managerId = loginInfo.id)
         val data = Data.of(ProductDetailInfo.toResponse(product))
         return CommonResponse.toResponse(data = data)
     }
 
     @DeleteMapping("/{product-id}")
-    fun delete(@LoginUser loginInfo: LoginInfo, @PathVariable("product-id") productId: Long): CommonResponse {
+    fun delete(
+        @LoginUser loginInfo: LoginInfo,
+        @PathVariable("product-id") productId: Long
+    ): CommonResponse {
         productWriteService.delete(productId = productId, managerId = loginInfo.id)
         return CommonResponse.toResponse()
     }
-    @PutMapping("/{product-id}")
-    fun update(@LoginUser loginInfo: LoginInfo, @PathVariable("product-id") productId: Long): CommonResponse {
 
-        return CommonResponse.toResponse()
+    @GetMapping
+    fun getProducts(@LoginUser loginInfo: LoginInfo, search: Search, @PageableDefault(page = 0, size = 10) pageable: Pageable): CommonResponse {
+        val products = productReadService.getProducts(loginInfo.id, search.keyword, page = pageable.pageNumber, limit = pageable.pageSize, offset = pageable.offset)
+        val data = Data.of(Products.of(products))
+        return CommonResponse.toResponse(data= data)
     }
 }
