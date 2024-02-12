3bc444a650462a37dd14abc3b84d9b65a6c6d8e3
cousim46 <wjdghldns24@naver.com>
2024-02-12 15:47:58 +0900

    fix : 불필요한 메서드 쿼리 삭제

--- src/main/kotlin/product/management/app/manager/ManagerRepository.kt
+++ src/main/kotlin/product/management/app/manager/ManagerRepository.kt
@@ -6,5 +6,4 @@ import product.management.app.manager.domain.Manager
 interface ManagerRepository : JpaRepository<Manager, Long> {
 
     fun findByPhone(phone: String): Manager?
-    fun existsByPhone(phone: String) : Boolean
 }
